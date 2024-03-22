f6d04a0e34459e5509049513325278bb23755094
Giyoul <cla12093@gmail.com>
2024-03-22 10:30:00 +0900

    :tada: Project begin!

---
+++ src/main/java/com/familycard/familycardback/FamilyCardBackApplication.java
@@ -0,0 +1,13 @@
+package com.familycard.familycardback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyCardBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyCardBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/familycard/familycardback/FamilyCardBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familycard.familycardback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyCardBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
