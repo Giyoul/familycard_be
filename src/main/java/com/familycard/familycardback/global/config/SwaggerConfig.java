61d7b212be8bb55200f5c425aa88cf3edb471dff
Giyoul <cla12093@gmail.com>
2024-06-30 12:34:56 +0900

    :wrench: Add swaggerConfig

---
+++ src/main/java/com/familycard/familycardback/global/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.familycard.familycardback.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${familyCard.server.domain}")
+    private String domain;
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("http://localhost:8080"))
+                .addServersItem(new Server().url(domain))
+                .info(new Info()
+                        .title("Family Card backend API")
+                        .description("패밀리 카드 api 명세")
+                        .version("0.9.0"));
+    }
+}
