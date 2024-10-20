f03ffb793724e3acf43af34575a2572d2b1210ad
Giyoul <cla12093@gmail.com>
2024-10-20 12:00:00 +0900

    :sparkles: Update menu info

--- src/main/java/com/familycard/familycardback/feature/menu/dto/response/MenuResponseDto.java
+++ src/main/java/com/familycard/familycardback/feature/menu/dto/response/MenuResponseDto.java
@@ -1,2 +1,18 @@
-package com.familycard.familycardback.feature.menu.dto.response;public class MenuResponseDto {
+package com.familycard.familycardback.feature.menu.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class MenuResponseDto {
+    @Getter
+    @Setter
+    public static class franchiseMenuComponent{
+        private String menuName;
+        private String menuPrice;
+
+        public franchiseMenuComponent(String menuName, String menuPrice) {
+            this.menuName = menuName;
+            this.menuPrice = menuPrice;
+        }
+    }
 }
--- src/main/java/com/familycard/familycardback/feature/menu/repository/MenuRepository.java
+++ src/main/java/com/familycard/familycardback/feature/menu/repository/MenuRepository.java
@@ -4,6 +4,9 @@ import com.familycard.familycardback.feature.menu.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Integer> {
+    Optional<Menu> findByMenuName(String menuName);
 }
