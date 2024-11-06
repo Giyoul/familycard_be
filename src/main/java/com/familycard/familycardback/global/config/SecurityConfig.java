b32d8438d70e16dd8b764bec0ec08da0fc7ef494
Giyoul <cla12093@gmail.com>
2024-11-06 21:24:46 +0900

    :lock: Update Security configuration

--- src/main/java/com/familycard/familycardback/global/config/SecurityConfig.java
+++ src/main/java/com/familycard/familycardback/global/config/SecurityConfig.java
@@ -38,37 +38,37 @@ public class SecurityConfig {
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/test", "/swagger-ui/**", "/v3/api-docs/**", "/api/login", "/api/register", "/error", "/api/health", "/actuator/health").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/**").permitAll()
-                        .requestMatchers(HttpMethod.DELETE, "/api/**").permitAll()
+//                        .requestMatchers(HttpMethod.DELETE, "/api/**").permitAll()
                         .requestMatchers(HttpMethod.PATCH, "/api/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
                         .requestMatchers(HttpMethod.PUT, "/api/**").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/api/**").permitAll()
                         .anyRequest().authenticated()
                 ).requiresChannel((channel) -> channel.anyRequest().requiresSecure()); // HTTP 요청을 HTTPS로 리디렉션
-
+//                );
         return http.build();
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.addAllowedOrigin("http://localhost:3000"); // 허용할 도메인 추가
-//        configuration.addAllowedOrigin("http://localhost:3001");
-//        configuration.addAllowedOrigin(serverDomain); // 환경 변수로 받은 도메인 추가
-//        configuration.addAllowedOrigin(clientDomain); // 환경 변수로 받은 도메인 추가
-//        configuration.addAllowedOrigin(serverDomain2);
-//        configuration.addAllowedMethod("*"); // 모든 메서드 허용
-//        configuration.addAllowedHeader("*"); // 모든 헤더 허용
-//        configuration.setAllowCredentials(true); // 자격 증명 허용
-//        configuration.setMaxAge(3600L); // 사전 요청 결과를 1시간 동안 캐시
+        configuration.addAllowedOrigin("http://localhost:3000"); // 허용할 도메인 추가
+        configuration.addAllowedOrigin("http://localhost:3001");
+        configuration.addAllowedOrigin(serverDomain); // 환경 변수로 받은 도메인 추가
+        configuration.addAllowedOrigin(clientDomain); // 환경 변수로 받은 도메인 추가
+        configuration.addAllowedOrigin(serverDomain2);
+        configuration.addAllowedMethod("*"); // 모든 메서드 허용
+        configuration.addAllowedHeader("*"); // 모든 헤더 허용
+        configuration.setAllowCredentials(true); // 자격 증명 허용
+        configuration.setMaxAge(3600L); // 사전 요청 결과를 1시간 동안 캐시
 //        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 //        source.registerCorsConfiguration("/**", configuration);
 //        return source;
 
-        configuration.addAllowedOriginPattern("*"); // 모든 도메인 허용
-        configuration.addAllowedMethod("*");
-        configuration.addAllowedHeader("*");
-        configuration.setAllowCredentials(true);
+//        configuration.addAllowedOriginPattern("*"); // 모든 도메인 허용
+//        configuration.addAllowedMethod("*");
+//        configuration.addAllowedHeader("*");
+//        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
